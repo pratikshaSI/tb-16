077ec8d2636100f3432052ca674de3d13d074e54
pratikshaSI <110393032+pratikshaSI@users.noreply.github.com>
2022-08-01 18:23:39 +0530

    Update demo.java
    demo1 for git

--- GITTB16/src/tb16/demo.java
+++ GITTB16/src/tb16/demo.java
@@ -1,7 +1,10 @@
 package tb16;
 
 public class demo {
-	
+	public void mi()
+	{
+	   System.out.println("welcome");	
+	}	
      public static void main(String[] args)
      {
 	   System.out.println("hello...git");	
